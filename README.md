b6f48f4ae45232c3fcb1ea522001ba74ef3ae735
Prabu Weerasinghe <prabu84@gmail.com>
2018-12-22 21:37:12 +0000

    Implement custom split func

--- README.md
+++ README.md
@@ -5,39 +5,18 @@ Also, I'm using this task to learn golang.
 
 Implementation objectives:
 
+Phase 1: Get words and counts (case sensitive + case insensitive)
+
 - ~~Basic implementation~~
 - ~~Add basic diagnostics to capture execution time~~
 - More fine grain control on splitting words
-- Create custom data structure (max-heap)
-- Create test harness to 
-    - verify implementation(s)
-    - Measure implementation speeds 
+    - Remove punctuation
+- Split implementations into multiple files
+- Run all implementations via harness that also does multiple iterations to average out timings
+- Implement case insensitive version
+    
+Phase 2: 
+     
+- Order results from phase 1 to find the most frequent word
+- Create custom data structure (max-heap) to improve performance on this aspect
 
-Personal objectives: 
-
-- Learn golang syntax and standard library
-- Learn golang unit testing framework
-
---------
-
-## Approaches
-
-### 1. Bufio.Scanner with bufio.ScanWords
-
- - In-built package from golang and easy to use.
- - Splits words by splitting text on empty space delimiter.
- - Excludes empty spaces.
- - Not the best at splitting words correctly. Includes punctuation with the word.
- 
- Notes about golang:
- 
- - No ternary operator.
- - Formatting is non-negotiable.
- - Documentation on standard lib is decent.
- - Defer adds functions to execute into a stack, and hence they are run LIFO once current function has finished executing.
- 
-### 2. Bufio.Scanner with custom split function 
-
-### 3. Bufio.Scanner with custom split function + Max heap
-
-### 4. Bufio.Scanner with custom split function + Max heap + Channels
--- word-frequency.go
+++ word-frequency.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"unicode"
+	"unicode/utf8"
 )
 
 func main() {
@@ -17,7 +19,9 @@ func main() {
 	defer printTimeTaken(now)
 
 	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanWords)
+
+	//scanner.Split(bufio.ScanWords)
+	scanner.Split(splitFunc)
 
 	maxWordLength := 0
 	wordCountMap := make(map[string]int)
@@ -36,6 +40,58 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func splitFunc (data []byte, atEOF bool) (advance int, token []byte, err error) {
+	start := 0
+	for width := 0; start < len(data); start += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[start:])
+		if !isSpace(r) || isSpecial(r) {
+			break
+		}
+	}
+	// Scan until space, marking end of word.
+	for width, i := 0, start; i < len(data); i += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[i:])
+		if isSpace(r) || isSpecial(r) {
+			return i + width, data[start:i], nil
+		}
+	}
+	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
+	if atEOF && len(data) > start {
+		return len(data), data[start:], nil
+	}
+	// Request more data.
+	return start, nil, nil
+}
+
+func isSpace(r rune) bool {
+	if r <= '\u00FF' {
+		// Obvious ASCII ones: \t through \r plus space. Plus two Latin-1 oddballs.
+		switch r {
+		case ' ', '\t', '\n', '\v', '\f', '\r':
+			return true
+		case '\u0085', '\u00A0':
+			return true
+		}
+		return false
+	}
+	// High-valued ones.
+	if '\u2000' <= r && r <= '\u200a' {
+		return true
+	}
+	switch r {
+	case '\u1680', '\u2028', '\u2029', '\u202f', '\u205f', '\u3000':
+		return true
+	}
+	return false
+}
+
+
+func isSpecial(r rune) bool {
+	return unicode.IsSymbol(r) || unicode.IsPunct(r) || unicode.IsNumber(r)
 }
 
 func printTimeTaken(now time.Time) {
@@ -57,4 +113,6 @@ func printMap(wordCountMap map[string]int, maxWordLength int){
 	for key, value := range wordCountMap {
 		fmt.Printf("%*v : %v\n", -maxWordLength, key, value)
 	}
+
+	fmt.Println(len(wordCountMap))
 }
